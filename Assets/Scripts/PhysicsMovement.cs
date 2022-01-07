be11fba8a4ab2421125f9d246036a35334763ea0
Shennod <96147981+Shennod@users.noreply.github.com>
2022-01-07 18:49:15 +0300

    CodeStyle and create Coroutine

--- Assets/Scripts/PhysicsMovement.cs
+++ Assets/Scripts/PhysicsMovement.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PhysicsMovement : MonoBehaviour
 {
     [SerializeField] private float _minGroundNormalY = .65f;
@@ -11,97 +14,100 @@ public class PhysicsMovement : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private SpriteRenderer _spriteRenderer;
 
-    protected Vector2 targetVelocity;
-    protected bool grounded;
-    protected Vector2 groundNormal;
-    protected Rigidbody2D rb2d;
-    protected ContactFilter2D contactFilter;
-    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+    private const string Idle = "Idle";
+    private const string Move = "Move";
 
-    protected const float minMoveDistance = 0.001f;
-    protected const float shellRadius = 0.01f;
+    protected Vector2 TargetVelocity;
+    protected bool Grounded;
+    protected Vector2 GroundNormal;
+    protected Rigidbody2D Rb2d;
+    protected ContactFilter2D ContactFilter;
+    protected RaycastHit2D[] HitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> HitBufferList = new List<RaycastHit2D>(16);
 
-    void OnEnable()
+    protected const float MinMoveDistance = 0.001f;
+    protected const float ShellRadius = 0.01f;
+
+    private void OnEnable()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        Rb2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Start()
+    private void Start()
     {
-        _animator.Play("Idle");
-        contactFilter.useTriggers = false;
-        contactFilter.SetLayerMask(_layerMask);
-        contactFilter.useLayerMask = true;
+        _animator.Play(Idle);
+        ContactFilter.useTriggers = false;
+        ContactFilter.SetLayerMask(_layerMask);
+        ContactFilter.useLayerMask = true;
     }
 
-    void Update()
+    private void Update()
     {
-        targetVelocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+        TargetVelocity = new Vector2(Input.GetAxis("Horizontal"), 0);
 
-        if(_velocity.x > 0)
+        if (_velocity.x > 0)
         {
             _spriteRenderer.flipX = false;
-            _animator.SetBool("Move", true);
+            _animator.SetBool(Move, true);
         }
-        else if(_velocity.x < 0)
+        else if (_velocity.x < 0)
         {
             _spriteRenderer.flipX = true;
-            _animator.SetBool("Move", true);
+            _animator.SetBool(Move, true);
         }
         else
         {
-            _animator.SetBool("Move", false);
+            _animator.SetBool(Move, false);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && Grounded)
             _velocity.y = 5;
     }
 
-    void FixedUpdate()
-    {      
+    private void FixedUpdate()
+    {
         _velocity += _gravityModifier * Physics2D.gravity * Time.deltaTime;
-        _velocity.x = targetVelocity.x;
+        _velocity.x = TargetVelocity.x;
 
-        grounded = false;
+        Grounded = false;
 
         Vector2 deltaPosition = _velocity * Time.deltaTime * _speed;
-        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 moveAlongGround = new Vector2(GroundNormal.y, -GroundNormal.x);
         Vector2 move = moveAlongGround * deltaPosition.x;
 
-        Movement(move, false);
+        Movements(move, false);
 
         move = Vector2.up * deltaPosition.y;
 
-        Movement(move, true);
+        Movements(move, true);
     }
 
-    void Movement(Vector2 move, bool yMovement)
+    private void Movements(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
 
-        if (distance > minMoveDistance)
+        if (distance > MinMoveDistance)
         {
-            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            int count = Rb2d.Cast(move, ContactFilter, HitBuffer, distance + ShellRadius);
 
-            hitBufferList.Clear();
+            HitBufferList.Clear();
 
             for (int i = 0; i < count; i++)
             {
-                hitBufferList.Add(hitBuffer[i]);
+                HitBufferList.Add(HitBuffer[i]);
             }
 
-            for (int i = 0; i < hitBufferList.Count; i++)
+            for (int i = 0; i < HitBufferList.Count; i++)
             {
-                Vector2 currentNormal = hitBufferList[i].normal;
+                Vector2 currentNormal = HitBufferList[i].normal;
                 if (currentNormal.y > _minGroundNormalY)
                 {
-                    grounded = true;
+                    Grounded = true;
                     if (yMovement)
                     {
-                        groundNormal = currentNormal;
+                        GroundNormal = currentNormal;
                         currentNormal.x = 0;
                     }
                 }
@@ -112,11 +118,11 @@ public class PhysicsMovement : MonoBehaviour
                     _velocity = _velocity - projection * currentNormal;
                 }
 
-                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                float modifiedDistance = HitBufferList[i].distance - ShellRadius;
                 distance = modifiedDistance < distance ? modifiedDistance : distance;
             }
         }
 
-        rb2d.position = rb2d.position + move.normalized * distance;
+        Rb2d.position = Rb2d.position + move.normalized * distance;
     }
 }
--- Assets/Scripts/Siren.cs
+++ Assets/Scripts/Siren.cs
@@ -8,7 +8,7 @@ public class Siren : MonoBehaviour
     [SerializeField] private AudioClip _audioClip;
 
     private AudioSource _audioSource;
-    private float _sirenVolume = 0;
+    private float _targetVolume = 0;
     private bool _thiefInHouse = false;
 
     private void Start()
@@ -21,23 +21,25 @@ public class Siren : MonoBehaviour
     {
         if (_thiefInHouse)
         {
-            if (_audioSource.volume == 0f)
-            {
-                _sirenVolume = 1f;
-            }
-            else if (_audioSource.volume == 1f)
-            {
-                _sirenVolume = 0f;
-            }
-
-            PlaySound();
+            StartCoroutine(PlaySound());
         }
     }
 
-    public void PlaySound()
+    private IEnumerator PlaySound()
     {
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _sirenVolume, Time.deltaTime);
-    }
+        if (_audioSource.volume == 0f)
+        {
+            _targetVolume = 1f;
+        }
+        else if (_audioSource.volume == 1f)
+        {
+            _targetVolume = 0f;
+        }
+
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, Time.deltaTime);
+
+        yield return null;
+    } 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -53,6 +55,7 @@ public class Siren : MonoBehaviour
         if (collision.TryGetComponent<PhysicsMovement>(out PhysicsMovement player))
         {
             _thiefInHouse = false;
+            StopCoroutine(PlaySound());
             _audioSource.Stop();
         }
     }
