fd8cbb0625691e98946f1ab91b5a5061db7f8dea
Shennod <96147981+Shennod@users.noreply.github.com>
2022-01-07 18:50:20 +0300

    code style

--- Assets/Scripts/Siren.cs
+++ Assets/Scripts/Siren.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(AudioSource))]
 public class Siren : MonoBehaviour
 {
     [SerializeField] private AudioClip _audioClip;
